6315c443b476f3e0c8d5fff734a3667647f768cf
rammelbalagtas <rammel.balagtas@gmail.com>
2022-03-11 09:54:18 -0500

    Replaced view controller with table view controller
    Replace view controller with table view controller
    Create table view cell class
    Create table view controller

---
+++ TodoAppCoreData/ToDoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ToDoTableViewCell.swift
+//  TodoAppCoreData
+//
+//  Created by Rammel on 2022-03-11.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
