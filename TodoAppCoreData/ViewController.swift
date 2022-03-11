f3439c6655114232eaa3a1025d7a4785fd070415
rammelbalagtas <rammel.balagtas@gmail.com>
2022-03-10 21:22:38 -0500

    Initial Commit

---
+++ TodoAppCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoAppCoreData
+//
+//  Created by Rammel on 2022-03-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
