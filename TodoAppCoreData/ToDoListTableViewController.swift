3e63c4adc1912d57a8d7a90a8321d405c04c2aa5
rammelbalagtas <rammel.balagtas@gmail.com>
2022-03-16 16:10:54 -0400

    Add core data functionality
    Implement core data functionality in the app. First, create the entity for the ToDo item. Instead of using the model, use CoreData for persisting data. When adding/removing entries, reload data has to be called to auto-update the table.

--- TodoAppCoreData/ToDoListTableViewController.swift
+++ TodoAppCoreData/ToDoListTableViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListTableViewController: UITableViewController {
     
-    var toDoList = ToDoList()
+    var persistentContainer: NSPersistentContainer!
+    var toDoList = [ToDo]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
         
@@ -21,12 +22,21 @@ class ToDoListTableViewController: UITableViewController {
          self.navigationItem.leftBarButtonItem = self.editButtonItem
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //fetch data from database
+        fetchData()
+        //reload table view
+        tableView.reloadData()
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return toDoList.list.count
+        return toDoList.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -34,15 +44,15 @@ class ToDoListTableViewController: UITableViewController {
             let cell = tableView.dequeueReusableCell(withIdentifier: "todo", for: indexPath) as? ToDoTableViewCell
         else {preconditionFailure("cannot deque reusable cell")}
         // Configure the cell...
-        let todo = toDoList.list[indexPath.row]
+        let todo = toDoList[indexPath.row]
         // add strikethrough for completed task
-        if todo.isComplete{
-            let title = NSAttributedString(string: todo.title, attributes:
+        if todo.isComplete {
+            let title = NSAttributedString(string: todo.title!, attributes:
                                         [.strikethroughStyle: 2,
                                          .strikethroughColor: UIColor.gray])
             cell.titleLabel?.attributedText = title
         } else {
-            let title = NSAttributedString(string: todo.title)
+            let title = NSAttributedString(string: todo.title!)
             cell.titleLabel?.attributedText = title
         }
         return cell
@@ -50,16 +60,26 @@ class ToDoListTableViewController: UITableViewController {
     
     // to toggle between complete and not complete
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        toDoList.list[indexPath.row].isComplete.toggle()
+        toDoList[indexPath.row].isComplete.toggle()
         tableView.reloadRows(at: [indexPath], with: .none)
     }
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source and table view
-            toDoList.deleteTodo(indexPath: indexPath)
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            //delete from data source
+            let moc = persistentContainer.viewContext
+            moc.delete(self.toDoList[indexPath.row])
+            moc.perform {
+                do {
+                    try moc.save()
+                } catch {
+                    moc.rollback()
+                }
+                //update table view
+                self.fetchData()
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
         }
     }
     
@@ -70,15 +90,36 @@ class ToDoListTableViewController: UITableViewController {
             field.placeholder = "Enter you todo item"
         }
         alert.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: nil))
-        alert.addAction(UIAlertAction(title: "Add", style: UIAlertAction.Style.default, handler: { _ in
+        alert.addAction(UIAlertAction(title: "Add", style: UIAlertAction.Style.default, handler: { [self] _ in
             guard let fields = alert.textFields else { return }
             let todoField = fields[0]
-            guard let todo = todoField.text, !todo.isEmpty else { return }
-            self.toDoList.list.insert(ToDo(title: todo), at: 0)
-            let indexPath = IndexPath(row: 0, section: 0)
-            self.tableView.insertRows(at: [indexPath], with: .top)
+            guard let title = todoField.text, !title.isEmpty else { return }
+    
+            //save todo
+            let moc = persistentContainer.viewContext
+            let todo = ToDo(context: moc)
+            todo.title = title
+            moc.perform {
+                do {
+                    try moc.save()
+                } catch {
+                    moc.rollback()
+                }
+                //update table view
+                self.fetchData()
+                tableView.reloadData()
+            }
         }))
         present(alert, animated: true, completion: nil)
+    }
+    
+    private func fetchData() {
+        let request: NSFetchRequest<ToDo> = ToDo.fetchRequest()
+        let moc = persistentContainer.viewContext
+        guard
+            let results = try? moc.fetch(request)
+        else {return}
+        toDoList = results
     }
     
 }
